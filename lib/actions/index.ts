f4b2072c987220ef8ba625bd871e23ccd63ac899
Yawar <rehaanyawer57@gmail.com>
2024-04-23 19:45:22 +0530

    added the search bar and its functionality

---
+++ lib/actions/index.ts
@@ -0,0 +1,16 @@
+'use server';
+import { connectToDB } from '../mongoose';
+import { scrapeAmazonProduct } from '../scrapper';
+export async function scrapeAndStoreProduct(productUrl: string) {
+  if (!productUrl) return;
+
+  try {
+    connectToDB();
+
+    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+    if (!scrapedProduct) return;
+  } catch (error: any) {
+    throw new Error(`Failed to create?update product:${error.message}`);
+  }
+}
