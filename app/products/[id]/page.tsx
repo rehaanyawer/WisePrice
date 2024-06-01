0534c1595e539ab7c4b42517c62db1a4abcd3a0e
Yawar <rehaanyawer57@gmail.com>
2024-06-01 11:56:40 +0530

    added cron jobs

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -161,7 +161,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
             </div>
           </div>
 
-          <Modal />
+          <Modal productId={id} />
         </div>
       </div>
 
